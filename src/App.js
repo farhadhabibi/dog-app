66d03634d7e078b0274c4a27bd96d2d4d9d0bff3
farhad-habibi <farhadmalekzada@gmail.com>
2023-01-10 14:55:50 +0500

    created the stylesheets for each component and added images

--- src/App.js
+++ src/App.js
@@ -1,25 +1,54 @@
-import logo from './logo.svg';
+import Navbar from './Navbar';
+import Routers from './Routers'
+import jack from './images/dog1.png';
+import whiskey from './images/dog2.png';
+import max from './images/dog3.png';
 import './App.css';
 
-function App() {
+function App(props) {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navbar dogs={props.dogs} />
+      <div className="container">
+        <Routers dogs={props.dogs} />
+      </div>
     </div>
   );
+}
+
+App.defaultProps = {
+  dogs: [
+    {
+      name: 'Jack',
+      age: 5,
+      src: jack,
+      facts: [
+        "Jack loves going outside",
+        "Jack likes eating chicken",
+        "Jacks likes to bark every night"
+      ]
+    },
+    {
+      name: 'Whiskey',
+      age: 4,
+      src: whiskey,
+      facts: [
+        "Whiskey is so smart and chubby",
+        "Whiskey wands to cuddle with his owner",
+        "Whiskey is highly inteligent"
+      ]
+    },
+    {
+      name: 'Max',
+      age: 3,
+      src: max,
+      facts: [
+        "Max is so kind",
+        "Max likes to eat meet",
+        "Max likes to walk out with you"
+      ]
+    },
+  ]
 }
 
 export default App;
